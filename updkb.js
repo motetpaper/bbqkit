775f36528c40d16d2528a5bfa781a5b1285050f2
MOTETPAPER <194758925+motetpaper@users.noreply.github.com>
2025-04-19 14:02:34 -0700

    updkb.js added

---
+++ updkb.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+// updkb.js
+// job    : updates the updated timestamp on kb articles
+// git    : https://github.com/motetpaper/bbqkit
+// lic    : MIT
+//
+//
+
+
+const fs = require('fs')
+
+const infile = process.argv[2];
+const outfile = infile;
+
+fs.readFile(infile, 'utf8', (err,data)=>{
+  const dt = new Date();
+  const upd = [
+    'updated:',
+    dt.toLocaleDateString('en-CA'),
+    dt.toLocaleTimeString('en-GB')
+  ].join(' ');
+
+  // updates the `updated:` field in Jekyll front matter
+  const re = /updated: .*/;
+  const outdata = '' + data.replace(re, upd);
+  console.log(`[updkb.js] ${upd} -> ${outfile}`);
+
+  fs.writeFile(outfile, outdata, (err)=>{
+    if (err) throw err;
+    console.log(`[updkb.js] Saved ${outfile}`)
+  });
+});

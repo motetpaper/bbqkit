4dda03860366187354a0b1e96b4f97fbdd94f95c
motetpaper <194758925+motetpaper@users.noreply.github.com>
2025-04-19 15:11:44 -0700

    Update updkb.js

--- updkb.js
+++ updkb.js
@@ -15,10 +15,12 @@ const outfile = infile;
 
 fs.readFile(infile, 'utf8', (err,data)=>{
   const dt = new Date();
+
+  const tz = 'UTC';
   const upd = [
     'updated:',
-    dt.toLocaleDateString('en-CA'),
-    dt.toLocaleTimeString('en-GB')
+    dt.toLocaleDateString('en-CA', { timeZone: tz }),
+    dt.toLocaleTimeString('en-GB', { timeZone: tz }),
   ].join(' ');
 
   // updates the `updated:` field in Jekyll front matter
